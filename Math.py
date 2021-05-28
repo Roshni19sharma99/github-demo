b3f1ebd1ffdb2033d11f404c65cfaea323d2ea26
Roshni sharma <44252772+Roshni19sharma99@users.noreply.github.com>
2021-05-28 14:57:05 +0530

    initial commit of math.py

---
+++ Math.py
@@ -0,0 +1,21 @@
+#add implementation
+def add(x,y):
+    return (x+y) 
+    
+#subtract implementation
+def subtract(x,y):
+    return (x+y)  #on master branch
+
+#mult implementation
+def multiply(x,y):
+    return (x*y)  #on master branch
+
+#divide implementation
+def divide(x,y):
+    return (x/y)  #on Bug456 branch
+def calculator:
+    if(x=0):
+        return DIVIDE_BY_0_ERROR  #on bug890 branch
+    else:
+        return (x/y)
+
